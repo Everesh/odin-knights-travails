8c1f6f4449080667b801e3be7adf36f502cce346
Everesh <everesh@proton.me>
2024-05-25 23:19:55 +0200

    Implement knight_moves

--- lib/knight.rb
+++ lib/knight.rb
@@ -3,8 +3,17 @@ class Knight
   MOVE_SET = [[2, -1], [2, 1], [1, 2], [-1, 2], [-2, -1], [-2, 1], [1, -2], [-1, -2]].freeze
 
   def knight_moves(start, finish)
+    queue = [[start, []]]
+    loop do
+      position = queue.shift
+      current_pos = position[0]
+      history = position[1] + [current_pos]
+      return history if history[-1] == finish
 
-    # TO DO
-
+      MOVE_SET.each do |move|
+        new_pos = [current_pos[0] + move[0], current_pos[1] + move[1]]
+        queue << [new_pos, history] if new_pos.all? { |pos| pos.between?(0, BOARD_SIZE - 1) }
+      end
+    end
   end
 end
