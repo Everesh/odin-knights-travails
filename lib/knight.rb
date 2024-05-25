5bcd98d5ee441ad0ac87c0608fdb27db8a061954
Everesh <everesh@proton.me>
2024-05-25 22:57:29 +0200

    Implement boilerplate

---
+++ lib/knight.rb
@@ -0,0 +1,10 @@
+class Knight
+  BOARD_SIZE = 8 # Represented as a width of a squre
+  MOVE_SET = [[2, -1], [2, 1], [1, 2], [-1, 2], [-2, -1], [-2, 1], [1, -2], [-1, -2]].freeze
+
+  def knight_moves(start, finish)
+
+    # TO DO
+
+  end
+end
